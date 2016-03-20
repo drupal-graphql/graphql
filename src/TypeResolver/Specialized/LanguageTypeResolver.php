7a9e9429a9733a098a22e5a1f5658ea3446c0fea
Sebastian Siemssen <fubhy@fubhy.com>
2016-03-20 14:50:57 +0100

    Make the language type schema serializable.

--- src/TypeResolver/Specialized/LanguageTypeResolver.php
+++ src/TypeResolver/Specialized/LanguageTypeResolver.php
@@ -54,48 +54,112 @@ class LanguageTypeResolver implements TypeResolverInterface {
         'Language', [
           'id' => [
             'type' => new NonNullModifier(Type::idType()),
-            'resolve' => function ($source) {
-              if ($source instanceof Language) {
-                return $source->getValue()->getId();
-              }
-            }
+            'resolve' => [__CLASS__, 'resolveId'],
           ],
           'name' => [
             'type' => new NonNullModifier(Type::stringType()),
-            'resolve' => function ($source) {
-              if ($source instanceof Language) {
-                return $source->getValue()->getName();
-              }
-            }
+            'resolve' => [__CLASS__, 'resolveName'],
           ],
           'direction' => [
             'type' => new NonNullModifier(Type::stringType()),
-            'resolve' => function ($source) {
-              if ($source instanceof Language) {
-                return $source->getValue()->getDirection();
-              }
-            }
+            'resolve' => [__CLASS__, 'resolveDirection'],
           ],
           'weight' => [
             'type' => new NonNullModifier(Type::intType()),
-            'resolve' => function ($source) {
-              if ($source instanceof Language) {
-                return $source->getValue()->getWeight();
-              }
-            }
+            'resolve' => [__CLASS__, 'resolveWeight'],
           ],
           'locked' => [
             'type' => new NonNullModifier(Type::booleanType()),
-            'resolve' => function ($source) {
-              if ($source instanceof Language) {
-                return $source->getValue()->isLocked();
-              }
-            }
+            'resolve' => [__CLASS__, 'resolveIsLocked'],
+          ],
+          'default' => [
+            'type' => new NonNullModifier(Type::booleanType()),
+            'resolve' => [__CLASS__, 'resolveIsDefault'],
           ],
         ]
       );
     }
 
     return $this->languageType;
+  }
+
+  /**
+   * Gets the name of the language.
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return string The human-readable name of the language (in the language that was
+   * The human-readable name of the language (in the language that was
+   * used to construct this object).
+   */
+  public static function resolveName(Language $language) {
+    return $language->getValue()->getName();
+  }
+
+  /**
+   * Gets the ID (language code).
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return string
+   *   The language code.
+   */
+  public static function resolveId(Language $language) {
+    return $language->getValue()->getId();
+  }
+
+  /**
+   * Gets the text direction (left-to-right or right-to-left).
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return string
+   *   Either self::DIRECTION_LTR or self::DIRECTION_RTL.
+   */
+  public static function resolveDirection(Language $language) {
+    return $language->getValue()->getDirection();
+  }
+
+  /**
+   * Gets the weight of the language.
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return int
+   *   The weight, used to order languages with larger positive weights sinking
+   *   items toward the bottom of lists.
+   */
+  public static function resolveWeight(Language $language) {
+    return $language->getValue()->getWeight();
+  }
+
+  /**
+   * Returns whether this language is the default language.
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return bool
+   *   Whether the language is the default language.
+   */
+  public static function resolveIsDefault(Language $language) {
+    return $language->getValue()->isDefault();
+  }
+
+  /**
+   * Returns whether this language is locked.
+   *
+   * @param \Drupal\Core\TypedData\Plugin\DataType\Language
+   *   The language object.
+   *
+   * @return bool
+   *   Whether the language is locked or not.
+   */
+  public static function resolveIsLocked(Language $language) {
+    return $language->getValue()->isLocked();
   }
 }
