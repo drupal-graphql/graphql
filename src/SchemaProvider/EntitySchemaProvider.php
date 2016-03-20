f9e6b1b94991933b18b5bbd171851606ad60e26d
Sebastian Siemssen <fubhy@fubhy.com>
2016-03-20 14:54:58 +0100

    Tweaking the entity schema provider.

--- src/SchemaProvider/EntitySchemaProvider.php
+++ src/SchemaProvider/EntitySchemaProvider.php
@@ -7,18 +7,18 @@
 
 namespace Drupal\graphql\SchemaProvider;
 
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\TypedData\EntityDataDefinitionInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\TypedData\TypedDataManager;
-use Drupal\field\Tests\FieldDefinitionIntegrityTest;
 use Drupal\graphql\TypeResolverInterface;
+use Drupal\graphql\Utility\String;
 use Fubhy\GraphQL\Language\Node;
 use Fubhy\GraphQL\Type\Definition\Types\EnumType;
 use Fubhy\GraphQL\Type\Definition\Types\ListModifier;
 use Fubhy\GraphQL\Type\Definition\Types\ModifierInterface;
-use Fubhy\GraphQL\Type\Definition\Types\NonNullModifier;
 use Fubhy\GraphQL\Type\Definition\Types\ObjectType;
 use Fubhy\GraphQL\Type\Definition\Types\ScalarType;
 use Fubhy\GraphQL\Type\Definition\Types\Type;
@@ -35,11 +35,15 @@ class EntitySchemaProvider extends SchemaProviderBase {
   protected $entityManager;
 
   /**
+   * The type resolver service.
+   *
    * @var \Drupal\graphql\TypeResolverInterface
    */
   protected $typeResolver;
 
   /**
+   * The typed data manager service.
+   *
    * @var \Drupal\Core\TypedData\TypedDataManager
    */
   protected $typedDataManager;
@@ -64,30 +68,53 @@ class EntitySchemaProvider extends SchemaProviderBase {
    * {@inheritdoc}
    */
   public function getQuerySchema() {
+    $fields = [];
+
     // We only support content entity types for now.
-    $entity_types = array_filter($this->entityManager->getDefinitions(), function (EntityTypeInterface $entity_type) {
+    $types = array_filter($this->entityManager->getDefinitions(), function (EntityTypeInterface $entity_type) {
       return $entity_type->isSubclassOf('\Drupal\Core\Entity\ContentEntityInterface');
     });
 
-    $fields = [];
-    foreach ($entity_types as $entity_type_id => $entity_type) {
-      /** @var \Drupal\Core\Entity\TypedData\EntityDataDefinition $definition */
-      $definition = $this->typedDataManager->createDataDefinition("entity:$entity_type_id");
-      $args = $this->getQueryArguments($definition);
+    // Format the entity type names as camel-cased strings.
+    $names = String::formatPropertyNameList(array_keys($types));
 
-      $fields[$entity_type_id] = [
-        'type' => new ListModifier($this->typeResolver->resolveRecursive($definition)),
-        'args' => $args,
-        'resolve' => [__CLASS__, 'resolveEntity'],
+    foreach ($types as $key => $type) {
+      /** @var \Drupal\Core\Entity\TypedData\EntityDataDefinition $definition */
+      $definition = $this->typedDataManager->createDataDefinition("entity:$key");
+      if (!$resolved = $this->typeResolver->resolveRecursive($definition)) {
+        continue;
+      }
+
+      $fields['id'][$names[$key]] = [
+        'type' => $resolved,
+        'args' => [
+          'id' => [
+            'type' => Type::intType(),
+            'required' => TRUE,
+          ],
+        ],
+        'resolve' => [__CLASS__, 'resolveFromId'],
+        'resolveData' => ['type' => $key],
+      ];
+
+      $fields['query'][$names[$key]] = [
+        'type' => new ListModifier($resolved),
+        'args' => $this->getQueryArguments($definition),
+        'resolve' => [__CLASS__, 'resolveFromEntityQuery'],
+        'resolveData' => ['type' => $key],
       ];
     }
 
-    return !empty($fields) ? ['entity' => [
-      'type' => new ObjectType('__EntityRoot', $fields),
-      'resolve' => function () {
-        return $this->entityManager;
-      }
-    ]] : [];
+    return !empty($fields) ? [
+      'entity' => [
+        'type' => new ObjectType('__EntityRoot', $fields['id']),
+        'resolve' => [__CLASS__, 'resolveRoot']
+      ],
+      'entityQuery' => [
+        'type' => new ObjectType('__EntityQueryRoot', $fields['query']),
+        'resolve' => [__CLASS__, 'resolveRoot']
+      ],
+    ] : [];
   }
 
   /**
@@ -109,7 +136,7 @@ class EntitySchemaProvider extends SchemaProviderBase {
 
       // Fetch the main property's definition and resolve it's type.
       $main_property_name = $storage->getMainPropertyName();
-      $main_property = $field_definition->getPropertyDefinition($main_property_name);
+      $main_property = $storage->getPropertyDefinition($main_property_name);
       $property_type = $this->typeResolver->resolveRecursive($main_property);
       $wrapped_type = $property_type;
 
@@ -134,6 +161,13 @@ class EntitySchemaProvider extends SchemaProviderBase {
   }
 
   /**
+   * @return bool
+   */
+  public static function resolveRoot() {
+    return TRUE;
+  }
+
+  /**
    * @param $source
    * @param array|NULL $args
    * @param $root
@@ -141,34 +175,50 @@ class EntitySchemaProvider extends SchemaProviderBase {
    *
    * @return array
    */
-  public static function resolveEntity($source, array $args = NULL, $root, Node $field) {
-    if ($source instanceof EntityManagerInterface) {
-      $args = array_filter($args, function ($arg) {
-        return isset($arg);
-      });
-
-      if (empty($args)) {
-        return [];
-      }
-
-      $storage = $source->getStorage($field->get('name')->get('value'));
-      $query = $storage->getQuery()
-        ->accessCheck(TRUE)
-        ->range(0, 10);
-
-      foreach ($args as $key => $arg) {
-        if (isset($arg)) {
-          $query->condition($key, $arg, 'IN');
-        }
-      }
-
-      if ($ids = $query->execute()) {
-        return array_map(function ($entity) {
-          return $entity->getTypedData();
-        }, $storage->loadMultiple($ids));
-      }
-
-      return [];
+  public static function resolveFromId($source, array $args = NULL, $root, Node $field, $a, $b, $c, $data) {
+    // @todo Fix injection of container dependencies in resolver functions.
+    $storage = \Drupal::entityManager()->getStorage($data['type']);
+    if ($entity = $storage->load($args['id'])) {
+      return $entity->getTypedData();
     }
+
+    return NULL;
+  }
+
+  /**
+   * @param $source
+   * @param array|NULL $args
+   * @param $root
+   * @param \Fubhy\GraphQL\Language\Node $field
+   *
+   * @return array
+   */
+  public static function resolveFromEntityQuery($source, array $args = NULL, $root, Node $field, $a, $b, $c, $data) {
+    $storage = \Drupal::entityManager()->getStorage($data['type']);
+    $query = $storage->getQuery()
+      ->accessCheck(TRUE)
+      // @todo Make this configurable and expose it as an argument.
+      ->range(0, 10);
+
+    foreach ($args as $key => $arg) {
+      if (isset($arg)) {
+        $operator = is_array($arg) ? 'IN' : '=';
+        $query->condition($key, $arg, $operator);
+      }
+    }
+
+    $result = $query->execute();
+    if (!empty($result)) {
+      $entities = $storage->loadMultiple($result);
+
+      // Filter entities that the current user doesn't have view access for.
+      return array_map(function (ContentEntityInterface $entity) {
+        return $entity->getTypedData();
+      }, array_filter($entities, function (ContentEntityInterface $entity) {
+        return $entity->access('view');
+      }));
+    }
+
+    return [];
   }
 }
