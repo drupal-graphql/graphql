bcea97a4a0202add6433b226479d40f3472bea13
Sebastian Siemssen <fubhy@fubhy.com>
2016-09-07 12:17:39 +0200

    Make cache and schema class configurable through services.yml.

--- src/GraphQL/Relay/Field/NodeField.php
+++ src/GraphQL/Relay/Field/NodeField.php
@@ -2,12 +2,17 @@
 
 namespace Drupal\graphql\GraphQL\Relay\Field;
 
+use Drupal\node\NodeInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Youshido\GraphQL\Execution\ResolveInfo;
 use Youshido\GraphQL\Relay\Fetcher\FetcherInterface;
 use Youshido\GraphQL\Relay\Field\NodeField as NodeFieldBase;
 use Youshido\GraphQL\Relay\Node;
+use Youshido\GraphQL\Relay\NodeInterfaceType;
+use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Type\Object\ObjectType;
+use Youshido\GraphQL\Type\Scalar\StringType;
 
 class NodeField extends NodeFieldBase implements ContainerAwareInterface, FetcherInterface {
 
@@ -33,7 +38,11 @@ class NodeField extends NodeFieldBase implements ContainerAwareInterface, Fetche
    * {@inheritdoc}
    */
   public function resolveNode($type, $id) {
-    // @todo Implement this.
+    switch ($type) {
+      case 'article':
+        return \Drupal::entityTypeManager()->getStorage('node')->load($id);
+    }
+
     throw new \Exception('The generic Relay node resolver is not yet implemented.');
   }
 
@@ -41,6 +50,25 @@ class NodeField extends NodeFieldBase implements ContainerAwareInterface, Fetche
    * {@inheritdoc}
    */
   public function resolveType($object) {
+    if ($object instanceof NodeInterface) {
+      switch ($object->bundle()) {
+        case 'sane_article':
+          return new ObjectType([
+            'name' => 'Article',
+            'fields' => [
+              'id' => new GlobalIdField('article'),
+              'title' => [
+                'type' => new NonNullType(new StringType()),
+                'resolve' => function (NodeInterface $node) {
+                  return $node->getTitle();
+                },
+              ],
+            ],
+            'interfaces' => [new NodeInterfaceType()],
+          ]);
+      }
+    }
+
     // @todo Implement this.
     throw new \Exception('The generic Relay type resolver is not yet implemented.');
   }
--- src/SchemaFactory.php
+++ src/SchemaFactory.php
@@ -35,24 +35,35 @@ class SchemaFactory {
   protected $schemaCache;
 
   /**
+   * The service configuration.
+   *
+   * @var array
+   */
+  protected $config;
+
+  /**
    * Constructs a SchemaFactory object.
    *
    * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
    *   The language manager service.
-   * @param \Drupal\graphql\SchemaProviderInterface $schema_provider
+   * @param \Drupal\graphql\SchemaProviderInterface $schemaProvider
    *   The schema provider service.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $schema_cache
+   * @param \Drupal\Core\Cache\CacheBackendInterface $schemaCache
    *   The schema cache backend.
+   * @param array $config
+   *   The configuration provided through the services.yml.
    */
-  public function __construct(LanguageManagerInterface $languageManager, SchemaProviderInterface $schema_provider, CacheBackendInterface $schema_cache) {
+  public function __construct(LanguageManagerInterface $languageManager, SchemaProviderInterface $schemaProvider, CacheBackendInterface $schemaCache, array $config) {
+    $this->config = $config;
+
     // Override the default type validator to enable services as field resolver
     // callbacks.
     $validator = ConfigValidator::getInstance();
     $validator->addRule('type', new TypeValidationRule($validator));
 
-    $this->schemaProvider = $schema_provider;
+    $this->schemaProvider = $schemaProvider;
     $this->languageManager = $languageManager;
-    $this->schemaCache = $schema_cache;
+    $this->schemaCache = $schemaCache;
   }
 
   /**
@@ -62,18 +73,22 @@ class SchemaFactory {
    *   The generated GraphQL schema.
    */
   public function getSchema() {
+    $useCache = $this->config['cache'];
     $language = $this->languageManager->getCurrentLanguage();
-    if ($schema = $this->schemaCache->get($language->getId())) {
+    if ($useCache && $schema = $this->schemaCache->get($language->getId())) {
       return $schema->data;
     }
 
+    $schemaClass = $this->config['schema_class'];
     $query = $this->schemaProvider->getQuerySchema();
     $mutation = $this->schemaProvider->getMutationSchema();
-    $schema = new Schema($query, $mutation);
+    $schema = new $schemaClass($query, $mutation);
 
-    // Cache the generated schema in the configured cache backend.
-    $tags  = ['views', 'entity_field_info', 'entity_bundles'];
-    $this->schemaCache->set($language->getId(), $schema, Cache::PERMANENT, $tags);
+    if ($useCache) {
+      // Cache the generated schema in the configured cache backend.
+      $tags = ['views', 'entity_field_info', 'entity_bundles'];
+      $this->schemaCache->set($language->getId(), $schema, Cache::PERMANENT, $tags);
+    }
 
     return $schema;
   }
