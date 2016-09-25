ff419faaae40307f616357f5d589899f17f22331
Daniel Wehner <dawehner@googlemail.com>
2016-09-25 16:44:16 +0100

    Add a super basic test

---
+++ tests/src/Functional/CustomSchemaTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Tests\graphql\Functional;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Tests some custom schema.
+ */
+class CustomSchemaTest extends QueryTestBase  {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['graphql_test_custom_schema'];
+
+  public function testSchema() {
+    Node::create([
+      'type' => 'article',
+      'title' => 'giraffe',
+    ])->save();
+
+    $query = <<<GQL
+{
+  articleById(id: 1) {
+    title
+  }
+}
+    
+GQL;
+
+    $body = $this->query($query);
+    $data = json_decode($body, TRUE);
+    $this->assertEquals([
+      'data' => [
+        'articleById' => [
+          'title' => 'giraffe',
+        ],
+      ],
+    ], $data);
+  }
+
+}
