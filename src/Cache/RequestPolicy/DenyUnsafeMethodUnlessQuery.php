22b290dd1ffa16de30b64762cbb3434f9264c03e
Sebastian Siemssen <fubhy@fubhy.com>
2016-12-07 17:35:37 +0100

    Overriding the core page cache request policy.

--- src/Cache/RequestPolicy/DenyUnsafeMethodUnlessQuery.php
+++ src/Cache/RequestPolicy/DenyUnsafeMethodUnlessQuery.php
@@ -12,27 +12,10 @@ use Symfony\Component\HttpFoundation\Request;
 class DenyUnsafeMethodUnlessQuery implements RequestPolicyInterface {
 
   /**
-   * The route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
-   * Constructs a new request policy instance.
-   *
-   * @param \Drupal\Core\Routing\RouteMatchInterface $routeMatch
-   *   The route provider service.
-   */
-  public function __construct(RouteMatchInterface $routeMatch) {
-    $this->routeMatch = $routeMatch;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function check(Request $request) {
-    if (!$request->isMethodSafe() && !($request->getMethod() === 'POST' && $this->routeMatch->getRouteName() === 'graphql.request')) {
+    if (!$request->isMethodSafe() && !($request->getMethod() === 'POST' && $request->getRequestUri() === '/graphql')) {
       return static::DENY;
     }
 
