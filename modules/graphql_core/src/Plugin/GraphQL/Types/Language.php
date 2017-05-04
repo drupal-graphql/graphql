cff0ae7a2c7088f2daefc16f7039ccd18165cfb3
Philipp Melab <philipp.melab@gmail.com>
2017-05-04 15:48:11 +0200

    Found another missing id!

--- modules/graphql_core/src/Plugin/GraphQL/Types/Language.php
+++ modules/graphql_core/src/Plugin/GraphQL/Types/Language.php
@@ -8,6 +8,7 @@ use Drupal\graphql_core\GraphQL\TypePluginBase;
  * GraphQL type for language objects.
  *
  * @GraphQLType(
+ *   id = "language",
  *   name = "Language",
  *   data_type = "language"
  * )
