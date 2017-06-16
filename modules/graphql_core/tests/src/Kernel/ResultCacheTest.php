c837248ddbcf067a97c7f46e1d707eadecd22fab
edwardaa <edward.xu2011@gmail.com>
2017-06-17 01:12:22 +0800

    Fixed cache for root fields

--- modules/graphql_core/tests/src/Kernel/ResultCacheTest.php
+++ modules/graphql_core/tests/src/Kernel/ResultCacheTest.php
@@ -17,11 +17,21 @@ class ResultCacheTest extends GraphQLFileTestBase {
   ];
 
   /**
-   * Test if the schema is created properly.
+   * Test if the result is cached properly.
    */
   public function testCachedQuery() {
     $first = $this->requestWithQueryFile('cache.gql');
     $second = $this->requestWithQueryFile('cache.gql');
+
+    $this->assertEquals($first, $second, 'Both runs return the same result.');
+  }
+
+  /**
+   * Test if the result is cached properly for root fields.
+   */
+  public function testRootFieldCachedQuery() {
+    $first = $this->requestWithQueryFile('cache_root_field.gql');
+    $second = $this->requestWithQueryFile('cache_root_field.gql');
 
     $this->assertEquals($first, $second, 'Both runs return the same result.');
   }
@@ -38,12 +48,34 @@ class ResultCacheTest extends GraphQLFileTestBase {
   }
 
   /**
+   * Test if a tag clear invalidates the cache of root fields.
+   */
+  public function testRootFieldTagClear() {
+    $first = $this->requestWithQueryFile('cache_root_field.gql');
+    $this->container->get('cache_tags.invalidator')->invalidateTags(['c']);
+    $second = $this->requestWithQueryFile('cache_root_field.gql');
+
+    $this->assertNotEquals($first, $second, 'Clearing the tag invalidated the cache.');
+  }
+
+  /**
    * Test if a context update invalidates the cache.
    */
   public function testContextChange() {
     $first = $this->requestWithQueryFile('cache.gql');
     $this->container->get('cache_context.graphql_test')->setContext(1);
     $second = $this->requestWithQueryFile('cache.gql');
+
+    $this->assertNotEquals($first, $second, 'Changing the context invalidated the cache.');
+  }
+
+  /**
+   * Test if a context update invalidates the cache of root fields.
+   */
+  public function testRootFieldContextChange() {
+    $first = $this->requestWithQueryFile('cache_root_field.gql');
+    $this->container->get('cache_context.graphql_test_root_field')->setContext(1);
+    $second = $this->requestWithQueryFile('cache_root_field.gql');
 
     $this->assertNotEquals($first, $second, 'Changing the context invalidated the cache.');
   }
