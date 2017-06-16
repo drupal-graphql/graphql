e8eb37c73a79ca3f502c7aaecedea8628a077776
edwardaa <edward.xu2011@gmail.com>
2017-06-17 00:35:03 +0800

    Fixed cache test

--- modules/graphql_core/tests/src/Kernel/ResultCacheTest.php
+++ modules/graphql_core/tests/src/Kernel/ResultCacheTest.php
@@ -30,9 +30,9 @@ class ResultCacheTest extends GraphQLFileTestBase {
    * Test if a tag clear invalidates the cache.
    */
   public function testTagClear() {
-    $first = $this->executeQueryFile('cache.gql');
+    $first = $this->requestWithQueryFile('cache.gql');
     $this->container->get('cache_tags.invalidator')->invalidateTags(['a']);
-    $second = $this->executeQueryFile('cache.gql');
+    $second = $this->requestWithQueryFile('cache.gql');
 
     $this->assertNotEquals($first, $second, 'Clearing the tag invalidated the cache.');
   }
@@ -41,9 +41,9 @@ class ResultCacheTest extends GraphQLFileTestBase {
    * Test if a context update invalidates the cache.
    */
   public function testContextChange() {
-    $first = $this->executeQueryFile('cache.gql');
+    $first = $this->requestWithQueryFile('cache.gql');
     $this->container->get('cache_context.graphql_test')->setContext(1);
-    $second = $this->executeQueryFile('cache.gql');
+    $second = $this->requestWithQueryFile('cache.gql');
 
     $this->assertNotEquals($first, $second, 'Changing the context invalidated the cache.');
   }
