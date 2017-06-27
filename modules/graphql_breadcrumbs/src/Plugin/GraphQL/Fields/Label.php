b21a9035bb30761d65a04485ce1079754a04c880
Tadej Basa <tadej.basa@gmail.com>
2017-06-27 16:48:41 +0200

    Exposing breadcrumbs.

---
+++ modules/graphql_breadcrumbs/src/Plugin/GraphQL/Fields/Label.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\graphql_breadcrumbs\Plugin\GraphQL\Fields;
+
+use Drupal\graphql_core\GraphQL\FieldPluginBase;
+use Drupal\link\LinkItemInterface;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+/**
+ * Retrieve a link text.
+ *
+ * @GraphQLField(
+ *   id = "link_text",
+ *   name = "text",
+ *   type = "String",
+ *   types = {"Link"}
+ * )
+ */
+class Label extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveValues($value, array $args, ResolveInfo $info) {
+    if ($value instanceof \Drupal\Core\Link) {
+      yield $value->getText();
+    }
+  }
+
+}
---
+++ modules/graphql_breadcrumbs/src/Plugin/GraphQL/Fields/Url.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\graphql_breadcrumbs\Plugin\GraphQL\Fields;
+
+use Drupal\graphql_core\GraphQL\FieldPluginBase;
+use Drupal\link\LinkItemInterface;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+/**
+ * Retrieve a link's route object.
+ *
+ * @GraphQLField(
+ *   id = "link_url",
+ *   name = "url",
+ *   type = "Url",
+ *   types = {"Link"}
+ * )
+ */
+class Url extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveValues($value, array $args, ResolveInfo $info) {
+    if ($value instanceof \Drupal\Core\Link) {
+      yield $value->getUrl();
+    }
+  }
+
+}
