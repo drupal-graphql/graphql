95aff487220b1dcaf89d35f690c9e37c2b49f64f
Philipp Melab <philipp.melab@gmail.com>
2017-07-06 13:09:24 +0200

    Added multi-value filter testcase.

--- modules/graphql_views/tests/src/Kernel/ViewsTestBase.php
+++ modules/graphql_views/tests/src/Kernel/ViewsTestBase.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\Tests\graphql_views\Kernel;
 
+use Drupal\field\Tests\EntityReference\EntityReferenceTestTrait;
 use Drupal\simpletest\ContentTypeCreationTrait;
 use Drupal\simpletest\NodeCreationTrait;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\taxonomy\Tests\TaxonomyTestTrait;
 use Drupal\Tests\graphql_core\Kernel\GraphQLFileTestBase;
 use Drupal\user\Entity\Role;
 
@@ -15,6 +19,8 @@ use Drupal\user\Entity\Role;
 abstract class ViewsTestBase extends GraphQLFileTestBase {
   use NodeCreationTrait;
   use ContentTypeCreationTrait;
+  use EntityReferenceTestTrait;
+  use TaxonomyTestTrait;
 
   /**
    * {@inheritdoc}
@@ -25,6 +31,7 @@ abstract class ViewsTestBase extends GraphQLFileTestBase {
     'filter',
     'text',
     'views',
+    'taxonomy',
     'graphql_content',
     'graphql_views',
     'graphql_views_test',
@@ -44,18 +51,46 @@ abstract class ViewsTestBase extends GraphQLFileTestBase {
     parent::setUp();
     $this->installEntitySchema('node');
     $this->installEntitySchema('view');
+    $this->installEntitySchema('taxonomy_term');
     $this->installConfig(['node', 'filter', 'views', 'graphql_views_test']);
     $this->installSchema('node', 'node_access');
     $this->createContentType(['type' => 'test']);
+    $this->createEntityReferenceField('node', 'test', 'field_tags', 'Tags', 'taxonomy_term');
+
+    Vocabulary::create([
+      'name' => 'Tags',
+      'vid' => 'tags',
+    ])->save();
 
     Role::load('anonymous')
       ->grantPermission('access content')
       ->save();
 
+    $terms = [];
+
+    $terms['A'] = Term::create([
+      'name' => 'Term A',
+      'vid' => 'tags',
+    ]);
+    $terms['A']->save();
+
+    $terms['B'] = Term::create([
+      'name' => 'Term B',
+      'vid' => 'tags',
+    ]);
+    $terms['B']->save();
+
+    $terms['C'] = Term::create([
+      'name' => 'Term C',
+      'vid' => 'tags',
+    ]);
+    $terms['C']->save();
+
     foreach ($this->letters as $index => $letter) {
       $this->createNode([
         'title' => 'Node ' . $letter,
         'type' => 'test',
+        'field_tags' => $terms[$letter],
       ])->save();
     }
   }
