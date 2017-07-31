0a18908d0049ad46133e41bfd28aaae4ac66170f
Sebastian Siemssen <fubhy@fubhy.com>
2017-07-31 16:27:04 +0200

    Removing condition. This can only ever be null for computed fields, which we don't support here anyways.

--- modules/graphql_content/src/Plugin/Deriver/RawValueFieldTypeDeriver.php
+++ modules/graphql_content/src/Plugin/Deriver/RawValueFieldTypeDeriver.php
@@ -2,16 +2,8 @@
 
 namespace Drupal\graphql_content\Plugin\Deriver;
 
-use Drupal\Component\Plugin\Derivative\DeriverBase;
-use Drupal\Core\Entity\ContentEntityTypeInterface;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
-use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
-use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\graphql_content\Plugin\GraphQL\Types\RawValueFieldType;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Derive GraphQL types for raw values of drupal fields.
@@ -22,11 +14,9 @@ class RawValueFieldTypeDeriver extends FieldFormatterDeriver {
    * {@inheritdoc}
    */
   protected function getDefinition($entityType, $bundle, array $displayOptions, FieldStorageDefinitionInterface $storage = NULL) {
-    if ($storage) {
-      return [
-        'name' => RawValueFieldType::getId($entityType, $storage->getName()),
-      ];
-    }
+    return [
+      'name' => RawValueFieldType::getId($entityType, $storage->getName()),
+    ];
   }
 
 }
