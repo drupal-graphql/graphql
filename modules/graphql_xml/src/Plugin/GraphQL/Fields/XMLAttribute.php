881809b77cf331d5c38bde73908e45c9eda3d3af
Philipp Melab <philipp.melab@gmail.com>
2017-08-21 15:08:22 +0200

    Pull information from xml data in text fields. (#197)

---
+++ modules/graphql_xml/src/Plugin/GraphQL/Fields/XMLAttribute.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\graphql_xml\Plugin\GraphQL\Fields;
+
+use Drupal\graphql_core\GraphQL\FieldPluginBase;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+/**
+ * Get an xml elements attribute value.
+ *
+ * @GraphQLField(
+ *   id = "xml_attribute",
+ *   type = "String",
+ *   name = "attribute",
+ *   arguments = { "name": "String" },
+ *   types = { "XMLElement" }
+ * )
+ */
+class XMLAttribute extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function resolveValues($value, array $args, ResolveInfo $info) {
+    if ($value instanceof \DOMElement) {
+      yield $value->getAttribute($args['name']);
+    }
+  }
+
+}
---
+++ modules/graphql_xml/src/Plugin/GraphQL/Fields/XMLField.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\graphql_xml\Plugin\GraphQL\Fields;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\graphql_core\GraphQL\FieldPluginBase;
+use Drupal\text\Plugin\Field\FieldType\TextItemBase;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+/**
+ * Expose text fields as xml documents.
+ *
+ * @GraphQLField(
+ *   id = "xml_field",
+ *   field_formatter = "graphql_xml",
+ *   type = "XMLElement",
+ *   deriver = "Drupal\graphql_content\Plugin\Deriver\FieldFormatterDeriver"
+ * )
+ */
+class XMLField extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function resolveValues($value, array $args, ResolveInfo $info) {
+    if ($value instanceof ContentEntityInterface) {
+      foreach ($value->get($this->getPluginDefinition()['field']) as $field) {
+        if ($field instanceof TextItemBase) {
+          $document = new \DOMDocument();
+          $document->loadXML('<div>' . $field->getValue()['value'] . '</div>');
+          yield $document->documentElement;
+        }
+      }
+    }
+  }
+
+}
