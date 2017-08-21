b1792dd6a170ad6a8f2e34e70cf4f81cf32b940f
Sebastian Siemssen <fubhy@fubhy.com>
2017-08-21 14:36:41 +0200

    Adding support for entity deletion and tests (#255)
    * Adding mutation for deleting entities.
    
    * Adding tests for entity creation.
    
    * Adding test for entity deletion.

---
+++ modules/graphql_content_mutation/src/Plugin/GraphQL/Fields/DeleteEntityOutputErrors.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\graphql_content_mutation\Plugin\GraphQL\Fields;
+
+use Drupal\graphql_content_mutation\Plugin\GraphQL\DeleteEntityOutputWrapper;
+use Drupal\graphql_core\GraphQL\FieldPluginBase;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+/**
+ * Retrieve a list of error messages.
+ *
+ * @GraphQLField(
+ *   id = "delete_entity_output_errors",
+ *   name = "errors",
+ *   type = "String",
+ *   types = {"DeleteEntityOutput"},
+ *   multi = true,
+ *   nullable = false
+ * )
+ */
+class DeleteEntityOutputErrors extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resolveValues($value, array $args, ResolveInfo $info) {
+    if ($value instanceof DeleteEntityOutputWrapper) {
+      if ($errors = $value->getErrors()) {
+        foreach ($errors as $error) {
+          yield $error;
+        }
+      }
+    }
+  }
+
+}
