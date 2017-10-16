a6894236749cac1f9586d1876790b7a70d8562c5
Sebastian Siemssen <fubhy@fubhy.com>
2017-10-16 13:10:09 +0200

    Use description from plugin annotation also for fields and enums.

--- src/Plugin/GraphQL/Enums/EnumPluginBase.php
+++ src/Plugin/GraphQL/Enums/EnumPluginBase.php
@@ -5,6 +5,7 @@ namespace Drupal\graphql\Plugin\GraphQL\Enums;
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\graphql\Plugin\GraphQL\PluggableSchemaManagerInterface;
 use Drupal\graphql\Plugin\GraphQL\Traits\CacheablePluginTrait;
+use Drupal\graphql\Plugin\GraphQL\Traits\NamedPluginTrait;
 use Drupal\graphql\Plugin\GraphQL\Traits\PluginTrait;
 use Drupal\graphql\Plugin\GraphQL\TypeSystemPluginInterface;
 use Youshido\GraphQL\Config\Object\EnumTypeConfig;
@@ -15,6 +16,7 @@ use Youshido\GraphQL\Type\Enum\AbstractEnumType;
  */
 abstract class EnumPluginBase extends AbstractEnumType implements TypeSystemPluginInterface {
   use PluginTrait;
+  use NamedPluginTrait;
   use CacheablePluginTrait;
 
   /**
@@ -41,7 +43,8 @@ abstract class EnumPluginBase extends AbstractEnumType implements TypeSystemPlug
    */
   public function buildConfig(PluggableSchemaManagerInterface $schemaManager) {
     $this->config = new EnumTypeConfig([
-      'name' => $this->getPluginDefinition()['name'],
+      'name' => $this->buildName(),
+      'description' => $this->buildDescription(),
       'values' => $this->buildValues($schemaManager),
     ]);
   }
