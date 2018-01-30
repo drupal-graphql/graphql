02c807d04d65ab753d5bb6156e2052bb08d36e9a
Philipp Melab <philipp.melab@gmail.com>
2018-01-30 15:14:32 +0100

    All remaining type system plugin mocks with test coverage.

---
+++ tests/src/Kernel/Framework/MockingFrameworkTest.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Drupal\Tests\graphql\Kernel\Framework;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\graphql\Plugin\GraphQL\PluggableSchemaBuilderInterface;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\graphql\Traits\ByPassAccessTrait;
+use Drupal\Tests\graphql\Traits\MockTypeSystemTrait;
+use Drupal\Tests\graphql\Traits\QueryTrait;
+use Drupal\Tests\graphql\Traits\MockSchemaTrait;
+use Youshido\GraphQL\Execution\ResolveInfo;
+
+class MockingFrameworkTest extends KernelTestBase {
+
+  use QueryTrait;
+  use ByPassAccessTrait;
+  use MockSchemaTrait;
+  use MockTypeSystemTrait;
+
+  public static $modules = [
+    'system',
+    'graphql',
+  ];
+
+  function getSchemaDefinitions() {
+    return [
+      'default' => [
+        'id' => 'default',
+        'name' => 'default',
+        'path' => 'graphql',
+      ],
+    ];
+  }
+
+  public function register(ContainerBuilder $container) {
+    parent::register($container);
+    $this->registerSchemaPluginManager($container);
+    $this->registerTypeSystemPluginManagers($container);
+  }
+
+  protected function setUp() {
+    parent::setUp();
+    $this->byPassAccess();
+    $this->installConfig('system');
+    $this->installConfig('graphql');
+  }
+
+  public function testFieldMock() {
+    $this->mockField('root', [
+      'name' => 'root',
+      'type' => 'String',
+    ], function ($value, array $args, ResolveInfo $info) {
+      yield 'test';
+    });
+
+    $result = $this->query('{ root }');
+    $this->assertEquals([
+      'data' => [
+        'root' => 'test',
+      ],
+    ], json_decode($result->getContent(), TRUE));
+  }
+
+  public function testTypeMock() {
+    $this->mockField('value', [
+      'name' => 'value',
+      'parents' => ['Test'],
+      'type' => 'String',
+    ], function ($value, array $args, ResolveInfo $info) {
+      yield $value['value'];
+    });
+
+    $this->mockType('test', [
+      'name' => 'Test',
+    ]);
+
+    $this->mockField('root', [
+      'name' => 'root',
+      'type' => 'Test',
+    ], function ($value, array $args, ResolveInfo $info) {
+      yield ['value' => 'test'];
+    });
+
+    $result = json_decode($this->query('{ root { value } }')->getContent(), TRUE);
+    $this->assertEquals([
+      'data' => [
+        'root' => ['value' => 'test'],
+      ],
+    ], $result);
+  }
+
+  public function testMutationMock() {
+    // Fake at least a root field, or the schema will return an error.
+    $this->mockField('root', [
+      'name' => 'root',
+      'type' => 'Boolean',
+    ], TRUE);
+
+    $this->mockEnum('gender', [
+      'name' => 'Gender',
+    ], function (PluggableSchemaBuilderInterface $builder) {
+      return [
+        ['value' => 'f', 'name' => 'Female', 'description' => ''],
+        ['value' => 'm', 'name' => 'Male', 'description' => ''],
+      ];
+    });
+
+    $this->mockInputType('user', [
+      'name' => 'User',
+      'fields' => [
+        'name' => 'String',
+        'age' => 'Int',
+        'gender' => 'Gender',
+      ],
+    ]);
+
+    $this->mockMutation('addUser', [
+      'name' => 'addUser',
+      'type' => 'Boolean',
+      'arguments' => [
+        'user' => 'User',
+      ],
+    ], function ($value, $args) {
+      return $args['user']['age'] > 50 && $args['user']['gender'] == 'm';
+    });
+
+    $result = json_decode($this->query('mutation ($user: User!) { addUser(user: $user) }', [
+      'user' => [
+        'name' => 'John Doe',
+        'age' => 52,
+        'gender' => 'Male',
+      ],
+    ])->getContent(), TRUE);
+
+    $this->assertEquals([
+      'data' => [
+        'addUser' => TRUE,
+      ],
+    ], $result);
+  }
+
+  public function testInterfaceMock() {
+
+    $this->mockInterface('token', [
+      'name' => 'Token',
+    ]);
+
+    $this->mockType('number', [
+      'name' => 'Number',
+      'interfaces' => ['Token'],
+    ], function ($value) {
+      return is_integer($value['value']);
+    });
+
+    $this->mockType('word', [
+      'name' => 'Word',
+      'interfaces' => ['Token'],
+    ], function ($value) {
+      return is_string($value['value']);
+    });
+
+    $this->mockField('int_value', [
+      'name' => 'value',
+      'type' => 'Int',
+      'parents' => ['Number'],
+    ], function ($value) { yield $value['value']; });
+
+    $this->mockField('string_value', [
+      'name' => 'value',
+      'type' => 'String',
+      'parents' => ['Word'],
+    ], function ($value) { yield $value['value']; });
+
+    $this->mockField('root', [
+      'name' => 'root',
+      'type' => 'Token',
+      'multi' => TRUE,
+    ], function () {
+      yield ['value' => 42];
+      yield ['value' => 'GraphQL'];
+    });
+
+    $result = json_decode($this->query('{ root { ... on Number { number:value } ... on Word { word:value }  } }')->getContent(), TRUE);
+
+    $this->assertEquals([
+      'data' => [
+        'root' => [
+          0 => ['number' => 42],
+          1 => ['word' => 'GraphQL'],
+        ],
+      ],
+    ], $result);
+  }
+
+  /**
+   * @todo Unions are identical to interfaces right now, but they should not be.
+   */
+  public function testUnionMock() {
+
+    $this->mockUnion('token', [
+      'name' => 'Token',
+    ]);
+
+    $this->mockType('number', [
+      'name' => 'Number',
+      'unions' => ['Token'],
+    ], function ($value) {
+      return is_integer($value['value']);
+    });
+
+    $this->mockType('word', [
+      'name' => 'Word',
+      'unions' => ['Token'],
+    ], function ($value) {
+      return is_string($value['value']);
+    });
+
+    $this->mockField('int_value', [
+      'name' => 'value',
+      'type' => 'Int',
+      'parents' => ['Number'],
+    ], function ($value) { yield $value['value']; });
+
+    $this->mockField('string_value', [
+      'name' => 'value',
+      'type' => 'String',
+      'parents' => ['Word'],
+    ], function ($value) { yield $value['value']; });
+
+    $this->mockField('root', [
+      'name' => 'root',
+      'type' => 'Token',
+      'multi' => TRUE,
+    ], function () {
+      yield ['value' => 42];
+      yield ['value' => 'GraphQL'];
+    });
+
+    $result = json_decode($this->query('{ root { ... on Number { number:value } ... on Word { word:value }  } }')->getContent(), TRUE);
+
+    $this->assertEquals([
+      'data' => [
+        'root' => [
+          0 => ['number' => 42],
+          1 => ['word' => 'GraphQL'],
+        ],
+      ],
+    ], $result);
+  }
+
+}
--- tests/src/Traits/MockTypeSystemTrait.php
+++ tests/src/Traits/MockTypeSystemTrait.php
@@ -5,11 +5,23 @@ namespace Drupal\Tests\graphql\Traits;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\graphql\Annotation\GraphQLEnum;
 use Drupal\graphql\Annotation\GraphQLField;
+use Drupal\graphql\Annotation\GraphQLInputType;
+use Drupal\graphql\Annotation\GraphQLInterface;
+use Drupal\graphql\Annotation\GraphQLMutation;
+use Drupal\graphql\Annotation\GraphQLType;
+use Drupal\graphql\Annotation\GraphQLUnionType;
+use Drupal\graphql\Plugin\GraphQL\Enums\EnumPluginBase;
 use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
+use Drupal\graphql\Plugin\GraphQL\InputTypes\InputTypePluginBase;
+use Drupal\graphql\Plugin\GraphQL\Interfaces\InterfacePluginBase;
+use Drupal\graphql\Plugin\GraphQL\Mutations\MutationPluginBase;
+use Drupal\graphql\Plugin\GraphQL\Types\TypePluginBase;
 use Drupal\graphql\Plugin\GraphQL\TypeSystemPluginInterface;
 use Drupal\graphql\Plugin\GraphQL\TypeSystemPluginManager;
 use Drupal\graphql\Plugin\GraphQL\TypeSystemPluginManagerInterface;
+use Drupal\graphql\Plugin\GraphQL\Unions\UnionTypePluginBase;
 use Drupal\KernelTests\KernelTestBase;
 use Prophecy\Argument;
 
@@ -79,11 +91,11 @@ trait MockTypeSystemTrait {
     }
   }
 
-  public function getTypeSystemPluginDefaults($annotationClass, $definition) {
+  protected function getTypeSystemPluginDefaults($annotationClass, $definition) {
     return (new $annotationClass($definition))->get();
   }
 
-  public function addTypeSystemPlugin(TypeSystemPluginInterface $plugin) {
+  protected function addTypeSystemPlugin(TypeSystemPluginInterface $plugin) {
     foreach ($this->typeSystemClassMap as $id => $class) {
       if ($plugin instanceof $class) {
         $this->typeSystemPlugins[$id][$plugin->getPluginId()] = $plugin;
@@ -91,31 +103,142 @@ trait MockTypeSystemTrait {
     }
   }
 
-  public function mockField($id, $definition, $resolver) {
-    $definition = $this->getTypeSystemPluginDefaults(GraphQLField::class, $definition + ['id' => $id]);
+  protected function toPromise($value) {
+    return $this->returnCallback(is_callable($value) ? $value : function () use ($value) {
+      yield $value;
+    });
+  }
+
+  protected function mockField($id, $definition, $result = NULL) {
+    $definition = $this->getTypeSystemPluginDefaults(
+     GraphQLField::class,
+     $definition + ['id' => $id]
+    );
 
     $field = $this->getMockBuilder(FieldPluginBase::class)
-      ->setConstructorArgs([[], 'root', $definition])
+      ->setConstructorArgs([[], $id, $definition])
       ->setMethods([
         'resolveValues',
       ])->getMock();
 
-    if (isset($resolver)) {
-      $resolve = $field
-        ->expects(static::any())
-        ->method('resolveValues')
-        ->with($this->anything(), $this->anything(), $this->anything());
-      if (is_callable($resolver)) {
-        $resolve->will($this->returnCallback($resolver));
-      }
-      else {
-        $resolve->will($this->returnCallback(function () use ($resolver) {
-          yield $resolver;
-        }));
-      }
-    }
+    $field
+      ->expects(static::any())
+      ->method('resolveValues')
+      ->with($this->anything(), $this->anything(), $this->anything())
+      ->will($this->toPromise($result));
+
+    $this->addTypeSystemPlugin($field);
 
     return $field;
+  }
+
+  protected function mockType($id, $definition, $applies = TRUE) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLType::class,
+      $definition + ['id' => $id]
+    );
+
+    $type = $this->getMockBuilder(TypePluginBase::class)
+      ->setConstructorArgs([[], $id, $definition])
+      ->setMethods([
+        'applies',
+      ])->getMock();
+
+    $type
+      ->expects(static::any())
+      ->method('applies')
+      ->with($this->anything(), $this->anything())
+      ->will($this->toPromise($applies));
+
+    $this->addTypeSystemPlugin($type);
+
+    return $type;
+  }
+
+  protected function mockInputType($id, $definition) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLInputType::class,
+      $definition + ['id' => $id]
+    );
+
+    $input = $this->getMockForAbstractClass(InputTypePluginBase::class, [[], $id, $definition]);
+
+    $this->addTypeSystemPlugin($input);
+
+    return $input;
+  }
+
+  protected function mockMutation($id, $definition, $result = NULL) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLMutation::class,
+      $definition + ['id' => $id]
+    );
+
+    $mutation = $this->getMockBuilder(MutationPluginBase::class)
+      ->setConstructorArgs([[], $id, $definition])
+      ->setMethods([
+        'resolve',
+      ])->getMock();
+
+    $mutation
+      ->expects(static::any())
+      ->method('resolve')
+      ->with($this->anything(), $this->anything(), $this->anything())
+      ->will($this->toPromise($result));
+
+    $this->addTypeSystemPlugin($mutation);
+
+    return $mutation;
+  }
+
+  protected function mockInterface($id, $definition) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLInterface::class,
+      $definition + ['id' => $id]
+    );
+
+    $interface = $this->getMockForAbstractClass(InterfacePluginBase::class, [[], $id, $definition]);
+
+    $this->addTypeSystemPlugin($interface);
+
+    return $interface;
+  }
+
+  protected function mockUnion($id, $definition) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLUnionType::class,
+      $definition + ['id' => $id]
+    );
+
+
+    $union = $this->getMockForAbstractClass(UnionTypePluginBase::class, [[], $id, $definition]);
+
+    $this->addTypeSystemPlugin($union);
+
+    return $union;
+  }
+
+  protected function mockEnum($id, $definition, $values = []) {
+    $definition = $this->getTypeSystemPluginDefaults(
+      GraphQLEnum::class,
+      $definition + ['id' => $id]
+    );
+
+    $enum = $this->getMockBuilder(EnumPluginBase::class)
+      ->setConstructorArgs([[], $id, $definition])
+      ->setMethods([
+        'buildValues',
+      ])->getMock();
+
+    $enum
+      ->expects(static::any())
+      ->method('buildValues')
+      ->with($this->anything())
+      ->will($this->toPromise($values));
+
+    $this->addTypeSystemPlugin($enum);
+
+    return $enum;
   }
 
 }
