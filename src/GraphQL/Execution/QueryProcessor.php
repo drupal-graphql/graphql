6dad48cf6ccf90c87a36220d6ac3bab323dc3bb4
Sebastian Siemssen <fubhy@fubhy.com>
2018-02-12 19:51:56 +0100

    Use cache max age from configuration.

--- src/GraphQL/Execution/QueryProcessor.php
+++ src/GraphQL/Execution/QueryProcessor.php
@@ -4,6 +4,7 @@ namespace Drupal\graphql\GraphQL\Execution;
 
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\graphql\GraphQL\Schema\SchemaLoader;
 
@@ -31,23 +32,34 @@ class QueryProcessor {
   protected $parameters;
 
   /**
+   * A config object for the system performance configuration.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $performanceConfig;
+
+  /**
    * QueryProcessor constructor.
    *
-   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
-   *   The current user.
    * @param \Drupal\graphql\GraphQL\Schema\SchemaLoader $schemaLoader
    *   The schema loader service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory service.
    * @param array $parameters
    *   The graphql container parameters.
    */
   public function __construct(
     SchemaLoader $schemaLoader,
     AccountProxyInterface $currentUser,
+    ConfigFactoryInterface $configFactory,
     array $parameters
   ) {
     $this->currentUser = $currentUser;
     $this->parameters = $parameters;
     $this->schemaLoader = $schemaLoader;
+    $this->performanceConfig = $configFactory->get('system.performance');
   }
 
   /**
@@ -94,6 +106,10 @@ class QueryProcessor {
     if ((!empty($request) && $request->hasMutations()) || $context->hasErrors()) {
       $responseCacheMetadata->setCacheMaxAge(0);
     }
+
+    // Add the configured cache max age from the page cache.
+    $maxAge = $this->performanceConfig->get('cache.page.max_age');
+    $responseCacheMetadata->mergeCacheMaxAge($maxAge);
 
     // Load the schema's cache metadata.
     $schemaCacheMetadata = $this->schemaLoader->getResponseCacheMetadata($schemaId);
