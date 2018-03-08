ba9d10738fca13642cf837ef8006b2d12e65a915
Sebastian Siemssen <fubhy@fubhy.com>
2018-03-08 18:22:20 +0100

    Use the path validator service to process the route field.

--- modules/graphql_core/src/Plugin/GraphQL/Fields/Routing/Route.php
+++ modules/graphql_core/src/Plugin/GraphQL/Fields/Routing/Route.php
@@ -3,11 +3,14 @@
 namespace Drupal\graphql_core\Plugin\GraphQL\Fields\Routing;
 
 use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
 use Drupal\graphql\GraphQL\Cache\CacheableValue;
 use Drupal\graphql\GraphQL\Execution\ResolveContext;
 use Drupal\graphql\Plugin\GraphQL\Fields\FieldPluginBase;
 use GraphQL\Type\Definition\ResolveInfo;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Retrieve a route object based on a path.
@@ -23,23 +26,53 @@ use GraphQL\Type\Definition\ResolveInfo;
  *   }
  * )
  */
-class Route extends FieldPluginBase {
+class Route extends FieldPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The path validator service.
+   *
+   * @var \Drupal\Core\Path\PathValidatorInterface
+   */
+  protected $pathValidator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('path.validator')
+    );
+  }
+
+  /**
+   * Route constructor.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $pluginId
+   *   The plugin id.
+   * @param mixed $pluginDefinition
+   *   The plugin definition.
+   * @param \Drupal\Core\Path\PathValidatorInterface $pathValidator
+   *   The path validator service.
+   */
+  public function __construct(array $configuration, $pluginId, $pluginDefinition, PathValidatorInterface $pathValidator) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition);
+    $this->pathValidator = $pathValidator;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function resolveValues($value, array $args, ResolveContext $context, ResolveInfo $info) {
-    if (UrlHelper::isExternal($args['path'])) {
-      yield Url::fromUri($args['path']);
+    if (($url = $this->pathValidator->getUrlIfValidWithoutAccessCheck($args['path'])) && $url->access()) {
+      yield $url;
     }
     else {
-      $url = Url::fromUri("internal:{$args['path']}", ['routed_path' => $args['path']]);
-      if ($url->isRouted() && $url->access()) {
-        yield $url;
-      }
-      else {
-        yield (new CacheableValue(NULL))->addCacheTags(['4xx-response']);
-      }
+      yield (new CacheableValue(NULL))->addCacheTags(['4xx-response']);
     }
   }
 
