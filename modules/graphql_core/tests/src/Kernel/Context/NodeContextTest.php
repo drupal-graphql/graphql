e0918ccddcc9e67429df5eebfc7945b513d17b3b
Philipp Melab <philipp.melab@gmail.com>
2018-03-09 09:45:58 +0100

    Added leaking cache metadata regression tests.

---
+++ modules/graphql_core/tests/src/Kernel/Context/NodeContextTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\graphql_core\Kernel\Context;
+
+use Drupal\node\Entity\Node;
+use Drupal\Tests\graphql_core\Kernel\GraphQLContentTestBase;
+
+/**
+ * Test full stack retrieval of a node context.
+ */
+class NodeContextTest extends GraphQLContentTestBase {
+
+  /**
+   * Regression test for unhandled logic exceptions.
+   *
+   * Leaking cache metadata.
+   */
+  public function testNodeContext() {
+    $nodeId = Node::create([
+      'title' => 'Test',
+      'type' => 'test',
+    ])->save();
+
+    $query = <<<GQL
+query (\$path: String!) {
+  route(path: \$path) {
+    ... on InternalUrl {
+      nodeContext {
+        entityLabel
+      }
+    }
+  }
+}
+GQL;
+
+    $this->query($query, ['path' => '/node/' . $nodeId]);
+  }
+
+}
