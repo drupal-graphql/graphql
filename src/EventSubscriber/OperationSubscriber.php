0cd8dfb2f6d5f8a832c7927b713c30c67e6a3272
Sebastian Siemssen <fubhy@fubhy.com>
2019-06-30 13:37:18 +0200

    Check if the language module is enabled. (#835)

--- src/EventSubscriber/OperationSubscriber.php
+++ src/EventSubscriber/OperationSubscriber.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\graphql\EventSubscriber;
 
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\Translator\TranslatorInterface;
 use Drupal\graphql\Event\OperationEvent;
 use Drupal\graphql\Plugin\LanguageNegotiation\OperationLanguageNegotiation;
-use Drupal\language\ConfigurableLanguageManagerInterface;
 use Drupal\language\LanguageNegotiatorInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -16,14 +16,23 @@ class OperationSubscriber implements EventSubscriberInterface {
   use CurrentLanguageResetTrait;
 
   /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
    * Constructs a OperationSubscriber object.
    *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
    * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
    * @param \Drupal\Core\StringTranslation\Translator\TranslatorInterface $translator
    * @param \Drupal\Core\Session\AccountInterface $currentUser
    * @param \Drupal\language\LanguageNegotiatorInterface $languageNegotiator
    */
-  public function __construct(LanguageManagerInterface $languageManager, TranslatorInterface $translator, AccountInterface $currentUser, LanguageNegotiatorInterface $languageNegotiator = NULL) {
+  public function __construct(ModuleHandlerInterface $moduleHandler, LanguageManagerInterface $languageManager, TranslatorInterface $translator, AccountInterface $currentUser, LanguageNegotiatorInterface $languageNegotiator = NULL) {
+    $this->moduleHandler = $moduleHandler;
     $this->languageManager = $languageManager;
     $this->translator = $translator;
     $this->currentUser = $currentUser;
@@ -37,7 +46,7 @@ class OperationSubscriber implements EventSubscriberInterface {
    *   The kernel event object.
    */
   public function onBeforeOperation(OperationEvent $event) {
-    if (!empty($this->languageNegotiator)) {
+    if ($this->moduleHandler->moduleExists('language') && !empty($this->languageNegotiator)) {
       OperationLanguageNegotiation::setContext($event->getContext());
     }
 
@@ -51,7 +60,7 @@ class OperationSubscriber implements EventSubscriberInterface {
    *   The kernel event object.
    */
   public function onAfterOperation(OperationEvent $event) {
-    if (!empty($this->languageNegotiator)) {
+    if ($this->moduleHandler->moduleExists('language') && !empty($this->languageNegotiator)) {
       OperationLanguageNegotiation::setContext($event->getContext());
     }
 
