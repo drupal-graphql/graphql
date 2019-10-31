9696cf80510743902c46c1721977558f24b9e6ea
Philipp Melab <philipp.melab@gmail.com>
2019-10-31 15:08:36 +0100

    Added comment to the entity query field why it is marked as insecure.

--- modules/graphql_core/src/Plugin/GraphQL/Fields/EntityQuery/EntityQuery.php
+++ modules/graphql_core/src/Plugin/GraphQL/Fields/EntityQuery/EntityQuery.php
@@ -37,6 +37,10 @@ use GraphQL\Type\Definition\ResolveInfo;
  *   },
  *   deriver = "Drupal\graphql_core\Plugin\Deriver\Fields\EntityQueryDeriver"
  * )
+ *
+ * This field is marked as not secure because it does not enforce entity field
+ * access over a chain of filters. For example node.uid.pass could be used as
+ * filter input which would disclose information about Drupal's password hashes.
  */
 class EntityQuery extends FieldPluginBase implements ContainerFactoryPluginInterface {
   use DependencySerializationTrait;
