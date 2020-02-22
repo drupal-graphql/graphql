9fb93b50fe8f09920aac38b85e1490067602fc50
Stefan Huber <sh@signalwerk.ch>
2020-02-22 10:38:00 +0100

    Add graphiql-explorer.(#971)

--- assets/explorer/src/index.js
+++ assets/explorer/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Drupal from 'drupal';
 import jQuery from 'jquery';
-import GraphiQL from 'graphiql';
 import { buildClientSchema } from 'graphql';
+import Explorer from './Explorer';
 
 /**
  * Behavior for rendering the GraphiQL interface.
@@ -20,23 +20,21 @@ Drupal.behaviors.graphQLRenderExplorer = {
     const graphQLSchema = buildClientSchema(settings.graphqlIntrospectionData.data);
 
     // Defines a GraphQL fetcher using the fetch API.
-    const graphQLFetcher = (graphQLParams) => fetch(settings.graphqlRequestUrl, {
+    const graphQLFetcher = graphQLParams => fetch(settings.graphqlRequestUrl, {
       method: 'post',
       credentials: 'same-origin',
       body: JSON.stringify(graphQLParams),
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then((response) => response.json());
+    }).then(response => response.json());
 
-    // Render <GraphiQL /> into the container.
-    ReactDOM.render(
-      React.createElement(GraphiQL, {
-        fetcher: graphQLFetcher,
-        schema: graphQLSchema,
-        query: settings.graphqlQuery || undefined,
-        variables: settings.graphqlVariables || undefined,
-      }), container
-    );
+    // Render <Explorer /> into the container.
+    ReactDOM.render(React.createElement(Explorer, {
+      fetcher: graphQLFetcher,
+      schema: graphQLSchema,
+      query: settings.graphqlQuery || undefined,
+      variables: settings.graphqlVariables || undefined,
+    }), container);
   },
 };
