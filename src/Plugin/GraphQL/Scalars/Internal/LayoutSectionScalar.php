eb76fc07d8f338209b59849535b1f414a93b55af
Peter Philipp <peter.philipp@cando-image.com>
2020-02-27 08:19:07 +1300

    #926 Add scalar type to handle the custom type used by LayoutBuilder

---
+++ src/Plugin/GraphQL/Scalars/Internal/LayoutSectionScalar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\graphql\Plugin\GraphQL\Scalars\Internal;
+
+
+/**
+ * Layout Builder module defines a custom data type that essentially is a
+ * string, but not called string.
+ *
+ * @GraphQLScalar(
+ *   id = "layout_section",
+ *   name = "layout_section",
+ *   type = "string"
+ * )
+ */
+class LayoutSectionScalar extends StringScalar {
+}
