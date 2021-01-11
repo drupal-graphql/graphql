9885d962f18f0ef21193865d80e9cc9032938a07
Корепов Алексей <korepov@digiterra.pro>
2021-01-11 22:31:21 +0300

    Fix Explorer with Layout Builder enabled

--- src/Plugin/GraphQL/Schemas/SchemaPluginBase.php
+++ src/Plugin/GraphQL/Schemas/SchemaPluginBase.php
@@ -439,7 +439,11 @@ abstract class SchemaPluginBase extends PluginBase implements SchemaPluginInterf
    * {@inheritdoc}
    */
   public function processFields(array $fields) {
-    return array_map([$this, 'buildField'], $fields);
+    $processFields = array_map([$this, 'buildField'], $fields);
+    foreach($processFields as $key => $processField) {
+      if($processField === false) unset($processFields[$key]);
+    }
+    return $processFields;
   }
 
   /**
@@ -506,6 +510,7 @@ abstract class SchemaPluginBase extends PluginBase implements SchemaPluginInterf
    *   The field definition.
    */
   protected function buildField($field) {
+    if($field['definition']['type'][0] == "layout_section") return false;
     if (!isset($this->fields[$field['id']])) {
       $creator = [$field['class'], 'createInstance'];
       $this->fields[$field['id']] = $creator($this, $this->fieldManager, $field['definition'], $field['id']);
