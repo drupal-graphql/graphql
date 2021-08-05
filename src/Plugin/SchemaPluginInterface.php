0368ccfc04276a0daeb217d93c59b9a864854b03
Max Pogonowski <max.pog@gmail.com>
2021-08-05 19:28:44 +1000

    Update src/Plugin/SchemaPluginInterface.php
    Co-authored-by: Alexander Varwijk <Kingdutch@users.noreply.github.com>

--- src/Plugin/SchemaPluginInterface.php
+++ src/Plugin/SchemaPluginInterface.php
@@ -17,7 +17,7 @@ interface SchemaPluginInterface extends PluginInspectionInterface, DerivativeIns
   /**
    * Set the serverID, required for cache id generation.
    *
-   * @param string
+   * @param string $serverId
    *   The machine name of the server using this plugin.
    */
   public function setServerId(string $serverId): void;
