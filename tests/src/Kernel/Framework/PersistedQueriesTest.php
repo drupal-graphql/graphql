bfc90b7817cb48916dbd999a3c5abb5aa40f4a18
Christian Fritsch <chr.fritsch@gmx.net>
2021-11-24 16:46:45 +0100

    test: add a test for the plugin

--- tests/src/Kernel/Framework/PersistedQueriesTest.php
+++ tests/src/Kernel/Framework/PersistedQueriesTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\graphql\Kernel\Framework;
 
 use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Tests the entire query result pipeline when using persisted queries.
@@ -51,6 +52,7 @@ GQL;
     $this->plugin_one = $manager->createInstance('persisted_query_plugin_one');
     $this->plugin_two = $manager->createInstance('persisted_query_plugin_two');
     $this->plugin_three = $manager->createInstance('persisted_query_plugin_three');
+    $this->plugin_apq = $manager->createInstance('automatic_persisted_query');
   }
 
   /**
@@ -113,6 +115,40 @@ GQL;
         ],
       ],
     ];
+  }
+
+  /**
+   * Test the automatic persisted queries plugin.
+   */
+  public function testAutomaticPersistedQueries(): void {
+    // Before adding the persisted query plugins to the server, we want to make
+    // sure that there are no existing plugins already there.
+    $this->server->removeAllPersistedQueryInstances();
+    $this->server->addPersistedQueryInstance($this->plugin_apq);
+    $this->server->save();
+
+    $endpoint = $this->server->get('endpoint');
+
+    $parameters['extensions']['persistedQuery']['sha256Hash'] = 'hash';
+
+    // Check we get PersistedQueryNotFound.
+    $request = Request::create($endpoint, 'GET', $parameters);
+    $result = $this->container->get('http_kernel')->handle($request);
+    $this->assertSame(200, $result->getStatusCode());
+    $this->assertSame(['errors' => [['message' => 'PersistedQueryNotFound', 'extensions' => ['category' => 'request']]]], json_decode($result->getContent(), TRUE));
+
+    // Post query to endpoint.
+    $content = json_encode(['query' => 'query { field_one } '] + $parameters);
+    $request = Request::create($endpoint, 'POST', [], [], [], [], $content);
+    $result = $this->container->get('http_kernel')->handle($request);
+    $this->assertSame(200, $result->getStatusCode());
+    $this->assertSame(['data' => ['field_one' => 'this is the field one']], json_decode($result->getContent(), TRUE));
+
+    // Execute first request again.
+    $request = Request::create($endpoint, 'GET', $parameters);
+    $result = $this->container->get('http_kernel')->handle($request);
+    $this->assertSame(200, $result->getStatusCode());
+    $this->assertSame(['data' => ['field_one' => 'this is the field one']], json_decode($result->getContent(), TRUE));
   }
 
 }
