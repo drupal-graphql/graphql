35ecbbfa2eee0bbe78fca85194c7cc5e46525063
Klaus Purer <klaus.purer@protonmail.ch>
2022-02-13 21:38:19 +0100

    test(dataprovider): Rename dataprovider functions to not be accidentally tested (#1266)

--- tests/src/Kernel/DataProducer/Images/ImageResourceUrlTest.php
+++ tests/src/Kernel/DataProducer/Images/ImageResourceUrlTest.php
@@ -14,7 +14,7 @@ class ImageResourceUrlTest extends GraphQLTestBase {
   /**
    * @covers \Drupal\graphql\Plugin\GraphQL\DataProducer\Images\ImageResourceUrl::resolve
    *
-   * @dataProvider testImageResourceUrlProvider
+   * @dataProvider imageResourceUrlProvider
    */
   public function testImageResourceUrl(array $input, string $expected): void {
     $result = $this->executeDataProducer('image_style_url', [
@@ -27,7 +27,7 @@ class ImageResourceUrlTest extends GraphQLTestBase {
   /**
    * Provider for testImageResourceUrl().
    */
-  public function testImageResourceUrlProvider(): array {
+  public function imageResourceUrlProvider(): array {
     return [
       [
         ['url' => 'http://localhost/test_image.jpg'],
