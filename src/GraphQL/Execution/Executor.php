6e24002e8288d8b69d7396a6e7328af3ecb59fc8
Christian Fritsch <chr.fritsch@gmx.net>
2022-02-28 15:44:54 +0100

    fix: moar stuff

--- src/GraphQL/Execution/Executor.php
+++ src/GraphQL/Execution/Executor.php
@@ -443,6 +443,7 @@ class Executor implements ExecutorImplementation {
    * @see \Drupal\Core\Cache\CacheBackendInterface::set()
    */
   protected function maxAgeToExpire($maxAge) {
+    /** @phpstan-ignore-next-line */
     $time = $this->requestStack->getMainRequest()->server->get('REQUEST_TIME');
     return ($maxAge === Cache::PERMANENT) ? Cache::PERMANENT : (int) $time + $maxAge;
   }
