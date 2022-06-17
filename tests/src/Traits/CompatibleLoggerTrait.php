39bce699ec7ae60a639bef5673e5c4f0a11c35d8
Klaus Purer <klaus.purer@protonmail.ch>
2022-06-17 13:36:54 +0200

    correct use statement

--- tests/src/Traits/CompatibleLoggerTrait.php
+++ tests/src/Traits/CompatibleLoggerTrait.php
@@ -4,9 +4,13 @@ namespace Drupal\Tests\graphql\Traits;
 
 use Drupal\Core\Logger\RfcLoggerTrait;
 
+// The logger trait has a different function signature in Drupal 10, so we need
+// this hack to register a compatible trait for both Drupal 9 and 10.
 [$version] = explode('.', \Drupal::VERSION, 2);
 if ($version >= 10) {
   eval(<<<'CODE'
+  use Drupal\Core\Logger\RfcLoggerTrait;
+
   trait CompatibleLoggerTrait {
     use RfcLoggerTrait;
 
