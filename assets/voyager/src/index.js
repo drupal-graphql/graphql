432ac47f8a4f092cf9cc847ca13522a85baacae1
Luis <luis.gil@biko2.com>
2023-08-22 10:37:21 +0200

    Replace jQuery.once by once drupal core function

--- assets/voyager/src/index.js
+++ assets/voyager/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Voyager } from 'graphql-voyager';
-import Drupal from 'drupal';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Voyager } from "graphql-voyager";
+import Drupal from "drupal";
 
 /**
  * Behavior for rendering the GraphQL Voyager interface.
  */
 Drupal.behaviors.graphQLRenderVoyager = {
   attach: (context, settings) => {
-    const container = once('#graphql-voyager', context) || undefined;
+    const container =
+      once("graphql-voyager", "#graphql-voyager", context)[0] || undefined;
 
-    if (typeof container === 'undefined') {
+    if (typeof container === "undefined") {
       return;
     }
 
