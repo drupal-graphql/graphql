112cdbd45c30e1fd6712f30d96015c5822675dac
Luis <luis.gil@biko2.com>
2023-08-22 11:14:59 +0200

    Update explorer and voyager to latest version

--- assets/explorer/src/index.js
+++ assets/explorer/src/index.js
@@ -1,46 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Drupal from "drupal";
-import GraphiQL from "graphiql";
-import { buildClientSchema } from "graphql";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Drupal from 'drupal';
+import jQuery from 'jquery';
+import { buildClientSchema } from 'graphql';
+import Explorer from './Explorer';
 
 /**
  * Behavior for rendering the GraphiQL interface.
  */
 Drupal.behaviors.graphQLRenderExplorer = {
   attach: (context, settings) => {
-    const container =
-      once("graphql-explorer", "#graphql-explorer", context)[0] || undefined;
+    const container = jQuery(once('graphql-explorer', '#graphql-explorer', context))[0] || undefined;
 
-    if (typeof container === "undefined") {
+    if (typeof container === 'undefined') {
       return;
     }
 
     // Build a schema from the passed introspection data.
-    const graphQLSchema = buildClientSchema(
-      settings.graphqlIntrospectionData.data
-    );
+    const graphQLSchema = buildClientSchema(settings.graphqlIntrospectionData.data);
 
     // Defines a GraphQL fetcher using the fetch API.
-    const graphQLFetcher = (graphQLParams) =>
-      fetch(settings.graphqlRequestUrl, {
-        method: "post",
-        credentials: "same-origin",
-        body: JSON.stringify(graphQLParams),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((response) => response.json());
+    const graphQLFetcher = graphQLParams => fetch(settings.graphqlRequestUrl, {
+      method: 'post',
+      credentials: 'same-origin',
+      body: JSON.stringify(graphQLParams),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json());
 
-    // Render <GraphiQL /> into the container.
-    ReactDOM.render(
-      React.createElement(GraphiQL, {
-        fetcher: graphQLFetcher,
-        schema: graphQLSchema,
-        query: settings.graphqlQuery || undefined,
-        variables: settings.graphqlVariables || undefined,
-      }),
-      container
-    );
+    // Render <Explorer /> into the container.
+    ReactDOM.render(React.createElement(Explorer, {
+      fetcher: graphQLFetcher,
+      schema: graphQLSchema,
+      query: settings.graphqlQuery || undefined,
+      variables: settings.graphqlVariables || undefined,
+    }), container);
   },
 };
--- assets/voyager/src/index.js
+++ assets/voyager/src/index.js
@@ -1,26 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Voyager } from "graphql-voyager";
-import Drupal from "drupal";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Voyager } from 'graphql-voyager';
+import Drupal from 'drupal';
+import jQuery from 'jquery';
+import once from '@drupal/once';
 
 /**
  * Behavior for rendering the GraphQL Voyager interface.
  */
 Drupal.behaviors.graphQLRenderVoyager = {
   attach: (context, settings) => {
-    const container =
-      once("graphql-voyager", "#graphql-voyager", context)[0] || undefined;
+    const container = jQuery(once('graphql-voyager', '#graphql-voyager', context))[0] || undefined;
 
-    if (typeof container === "undefined") {
+    if (typeof container === 'undefined') {
       return;
     }
 
-    ReactDOM.render(
-      <Voyager
-        introspection={settings.graphqlIntrospectionData}
-        displayOptions={{ skipRelay: true, sortByAlphabet: true }}
-      />,
-      container
-    );
+    ReactDOM.render(<Voyager
+      introspection={settings.graphqlIntrospectionData}
+      displayOptions={{ skipRelay: true, sortByAlphabet: true }}
+    />, container);
   },
 };
