1017cbc221bf8013a12a41b52324cadbdf20d250
Klaus Purer <klaus.purer@protonmail.ch>
2023-11-10 09:06:31 +0100

    docs(entity_access): Add param comments to entity_access data producer (#1374)

--- src/Plugin/GraphQL/DataProducer/Entity/EntityAccess.php
+++ src/Plugin/GraphQL/DataProducer/Entity/EntityAccess.php
@@ -38,10 +38,16 @@ class EntityAccess extends DataProducerPluginBase {
    * Resolver.
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to check access for.
    * @param string $operation
+   *   The access operation, for example "view".
    * @param \Drupal\Core\Session\AccountInterface $user
+   *   The user account access should be checked for.
+   * @param \Drupal\graphql\GraphQL\Execution\FieldContext $context
+   *   The context to add caching information to.
    *
    * @return bool
+   *   TRUE when access to the entity is allowed, FALSE otherwise.
    */
   public function resolve(EntityInterface $entity, ?string $operation, ?AccountInterface $user, FieldContext $context) {
     $result = $entity->access($operation ?? 'view', $user, TRUE);
