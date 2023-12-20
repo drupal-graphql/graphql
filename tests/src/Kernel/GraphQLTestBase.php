b2def711f69031bae2ca3ec379047920e44baaa4
Christian Fritsch <chr.fritsch@gmx.net>
2023-12-20 12:24:26 +0100

    Set language label in test

--- tests/src/Kernel/GraphQLTestBase.php
+++ tests/src/Kernel/GraphQLTestBase.php
@@ -82,11 +82,13 @@ abstract class GraphQLTestBase extends KernelTestBase {
     ConfigurableLanguage::create([
       'id' => 'fr',
       'weight' => 1,
+      'label' => 'french',                    
     ])->save();
 
     ConfigurableLanguage::create([
       'id' => 'de',
       'weight' => 2,
+      'label' => 'german', 
     ])->save();
 
     $this->builder = new ResolverBuilder();
