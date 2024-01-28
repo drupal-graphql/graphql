decf1a0ac24d005376b17b2af90c86911f7532ee
Klaus Purer <klaus.purer@protonmail.ch>
2024-01-28 11:57:02 +0100

    phpstan fixes

--- tests/src/Kernel/DataProducer/EntityReferenceTest.php
+++ tests/src/Kernel/DataProducer/EntityReferenceTest.php
@@ -5,8 +5,20 @@ namespace Drupal\Tests\graphql\Kernel\DataProducer;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
-use Drupal\Tests\field\Traits\EntityReferenceTestTrait;
 use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
+
+// @todo Drupal 9 compatibility: use the deprecated trait for Drupal 9.
+if (strpos(\Drupal::VERSION, '9') === 0) {
+  trait EntityReferenceFieldCreationTrait {
+    // @phpstan-ignore-next-line
+    use \Drupal\Tests\field\Traits\EntityReferenceTestTrait;
+  }
+}
+else {
+  trait EntityReferenceFieldCreationTrait {
+    use \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait;
+  }
+}
 
 /**
  * Tests the entity_reference data producers.
@@ -14,7 +26,7 @@ use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
  * @group graphql
  */
 class EntityReferenceTest extends GraphQLTestBase {
-  use EntityReferenceTestTrait;
+  use EntityReferenceFieldCreationTrait;
 
   /**
    * Test node that will be referenced.
