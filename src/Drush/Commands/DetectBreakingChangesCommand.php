2db2fed6d9f5408f4619e63160cebf78fd10bdb7
Alexander Varwijk <22619-Kingdutch@users.noreply.drupalcode.org>
2026-04-26 11:33:14 +0000

    feat: #3575840 Add Drush commands for schema dump and breaking changes detection
    By: Kingdutch
    By: dan2k3k4

---
+++ src/Drush/Commands/DetectBreakingChangesCommand.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\graphql\Drush\Commands;
+
+use Consolidation\OutputFormatters\FormatterManager;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\graphql\Entity\Server;
+use Drush\Attributes as CLI;
+use Drush\Commands\AutowireTrait;
+use Drush\Formatters\FormatterTrait;
+use GraphQL\Language\Source;
+use GraphQL\Utils\BreakingChangesFinder;
+use GraphQL\Utils\BuildClientSchema;
+use GraphQL\Utils\BuildSchema;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Path;
+
+/**
+ * Symfony command to detect breaking changes between current and stored schema.
+ */
+#[AsCommand(
+  name: self::NAME,
+  description: 'Detect breaking changes in the current schema compared to a previous schema.',
+  aliases: ['graphql:breaking']
+)]
+#[CLI\Formatter(returnType: RowsOfFields::class, defaultFormatter: 'table')]
+#[CLI\FieldLabels(labels: [
+  'type' => 'Type',
+  'description' => 'Description',
+])]
+#[CLI\DefaultTableFields(fields: ['type', 'description'])]
+#[CLI\FilterDefaultField(field: 'type')]
+final class DetectBreakingChangesCommand extends Command {
+
+  use AutowireTrait;
+  use FormatterTrait;
+
+  /**
+   * The command name.
+   */
+  const NAME = 'graphql:detect-breaking-changes';
+
+  /**
+   * Constructs a DumpSchemaCommand object.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+    private readonly LoggerInterface $logger,
+    // This is needed by `FormatterTrait`.
+    // @phpstan-ignore-next-line property.onlyWritten
+    private readonly FormatterManager $formatterManager,
+  ) {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure(): void {
+    $this
+      ->addArgument('server', InputArgument::REQUIRED, 'The GraphQL server to dump the schema for.')
+      ->addArgument('file', InputArgument::REQUIRED, "The JSON file to compare to.")
+      ->addOption('json', 'j', InputOption::VALUE_NONE, "Import previous schema as JSON, uses SDL if omitted.");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(InputInterface $input, OutputInterface $output): int {
+    $server_id = $input->getArgument('server');
+    $server = $this->entityTypeManager->getStorage('graphql_server')
+      ->load($server_id);
+    if ($server === NULL) {
+      throw new \InvalidArgumentException("Unknown GraphQL server '$server_id'.");
+    }
+    assert($server instanceof Server);
+
+    $schema = $server->configuration()->getSchema();
+    if ($schema === NULL) {
+      throw new \InvalidArgumentException("GraphQL server does not have a schema.");
+    }
+
+    $file = Path::makeAbsolute($input->getArgument('file'), getcwd());
+    if (!file_exists($file)) {
+      throw new \InvalidArgumentException("File '$file' does not exist.");
+    }
+
+    $contents = file_get_contents($file);
+    if ($contents === FALSE) {
+      throw new \InvalidArgumentException("File '$file' could not be read.");
+    }
+
+    $json = $input->getOption('json');
+
+    $extension = pathinfo($file, PATHINFO_EXTENSION);
+    if ($json && $extension !== 'json') {
+      $this->logger->warning("Command run with '--json' flag but file extension is not '.json' (got '.$extension'), this may cause parsing errors.");
+    }
+    elseif (!$json && $extension !== 'sdl') {
+      $this->logger->warning("Command run without '--json' flag but file extension is not '.sdl' (got '.$extension'), this may cause parsing errors.");
+    }
+
+    try {
+      $oldSchema = $json
+        ? BuildClientSchema::build(json_decode($contents, TRUE, 512, JSON_THROW_ON_ERROR))
+        : BuildSchema::build(new Source($contents, $file), NULL, ['assumeValid' => TRUE]);
+    }
+    catch (\Throwable $exception) {
+      throw new \RuntimeException("There was an error parsing $file", 0, $exception);
+    }
+
+    $changes = BreakingChangesFinder::findBreakingChanges($oldSchema, $schema);
+
+    if ($changes !== []) {
+      $this->writeFormattedOutput($input, $output, new RowsOfFields($changes));
+      return Command::FAILURE;
+    }
+
+    return Command::SUCCESS;
+  }
+
+}
---
+++ src/Drush/Commands/DumpSchemaCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\graphql\Drush\Commands;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\graphql\Entity\Server;
+use Drush\Commands\AutowireTrait;
+use GraphQL\Type\Introspection;
+use GraphQL\Utils\SchemaPrinter;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Path;
+
+/**
+ * Symfony command to dump the GraphQL schema to disk.
+ */
+#[AsCommand(
+  name: self::NAME,
+  description: 'Dump the GraphQL schema to disk.',
+)]
+final class DumpSchemaCommand extends Command {
+
+  use AutowireTrait;
+
+  /**
+   * The command name.
+   */
+  const NAME = 'graphql:dump';
+
+  /**
+   * Constructs a DumpSchemaCommand object.
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityTypeManager,
+  ) {
+    parent::__construct();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure(): void {
+    $this
+      ->addArgument('server', InputArgument::REQUIRED, 'The GraphQL server to dump the schema for.')
+      ->addArgument('file', InputArgument::OPTIONAL, "A filename to print write the schema to. Prints to stdout if omitted.")
+      ->addOption('json', 'j', InputOption::VALUE_NONE, "Print in JSON format instead.");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(InputInterface $input, OutputInterface $output): int {
+    $server_id = $input->getArgument('server');
+    $server = $this->entityTypeManager->getStorage('graphql_server')
+      ->load($server_id);
+    if ($server === NULL) {
+      throw new \InvalidArgumentException("Unknown GraphQL server '$server_id'.");
+    }
+    assert($server instanceof Server);
+
+    $schema = $server->configuration()->getSchema();
+    if ($schema === NULL) {
+      throw new \InvalidArgumentException("GraphQL server does not have a schema");
+    }
+
+    $file = $input->getArgument('file');
+    $json = $input->getOption('json');
+
+    $printed = $json ? json_encode(Introspection::fromSchema($schema), JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR) : SchemaPrinter::doPrint($schema);
+
+    if ($file !== NULL) {
+      $file = Path::makeAbsolute($file, getcwd());
+      file_put_contents($file, $printed);
+      fwrite(STDERR, "Wrote to $file" . PHP_EOL);
+    }
+    else {
+      $output->writeln($printed);
+    }
+
+    return Command::SUCCESS;
+  }
+
+}
